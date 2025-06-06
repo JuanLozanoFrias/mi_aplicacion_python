78a4e782ec8f0373dc17ee8de694ba98e5f2a239
JuanDavidLozano <juadlozanofri@gmail.com>
2025-06-05 21:34:52 -0500

    Actualiza app.py con los últimos cambios

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("¡Hola, mundo!")
+def pedir_nombre():
+    """Solicita al usuario que introduzca su nombre y lo devuelve."""
+    nombre = input("Introduce tu nombre: ")
+    return nombre
+
+if __name__ == "__main__":
+    nombre_usuario = pedir_nombre()
+    print(f"¡Hola, {nombre_usuario}!")
